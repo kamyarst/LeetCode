e3b4b453f86fdeab95574b01bdf3182693636a3f
Kamyar Sehati <kamyarsehati@icloud.com>
2022-05-30 10:27:56 +0430

    First commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LeetCode",
+    products: [
+        .library(
+            name: "LeetCode",
+            targets: ["LeetCode"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "LeetCode",
+            dependencies: []),
+        .testTarget(
+            name: "Dynamic Programming",
+            dependencies: ["LeetCode"]),
+    ]
+)
