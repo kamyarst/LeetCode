91c44f830c77aafb8af5037f6bae893f8ead3f86
Kamyar Sehati <kamyarsehati@icloud.com>
2022-05-30 10:34:27 +0430

    Implement edge values

--- Tests/Dynamic Programming/Day 1/FibonacciNumberTests.swift
+++ Tests/Dynamic Programming/Day 1/FibonacciNumberTests.swift
@@ -8,6 +8,24 @@
 import XCTest
 
 class FibonacciNumberTests: XCTestCase {
+    func test_minimumInputSuccess() {
+        let n = 0
+        let sut = self.makeSUT()
+
+        let result = sut.fib(n)
+
+        XCTAssertEqual(result, 0)
+    }
+
+    func test_maximumInputSuccess() {
+        let n = 30
+        let sut = self.makeSUT()
+
+        let result = sut.fib(n)
+
+        XCTAssertEqual(result, 832_040)
+    }
+
     func test_minimumInputFailure() {
         let n = -1
         let sut = self.makeSUT()
@@ -36,6 +54,7 @@ class FibonacciNumberTests: XCTestCase {
 struct FibonacciNumber {
     func fib(_ n: Int) -> Int? {
         guard n >= 0, n <= 30 else { return nil }
-        return 0
+        guard ![0, 1].contains(n) else { return n }
+        return -1
     }
 }
