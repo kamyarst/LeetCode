0c3ff3a47cd451375edf401099c2f546f36d7607
Kamyar Sehati <kamyarsehati@icloud.com>
2022-05-30 12:24:43 +0430

    Implement Fibonacci with Dynamic programming method

--- Tests/Dynamic Programming/Day 1/FibonacciNumberTests.swift
+++ Tests/Dynamic Programming/Day 1/FibonacciNumberTests.swift
@@ -5,6 +5,7 @@
 //  Created by adanic on 5/30/22.
 //
 
+@testable import LeetCode
 import XCTest
 
 class FibonacciNumberTests: XCTestCase {
@@ -13,76 +14,68 @@ class FibonacciNumberTests: XCTestCase {
         let n = 2
         let sut = self.makeSUT()
 
-        let result = sut.fib(n)
+        let result = sut.map { $0.fib(n) }
 
-        XCTAssertEqual(result, 1)
+        XCTAssertTrue(result.allSatisfy { $0 == 1 })
     }
 
     func test_leetCodeExample2() {
         let n = 3
         let sut = self.makeSUT()
 
-        let result = sut.fib(n)
+        let result = sut.map { $0.fib(n) }
 
-        XCTAssertEqual(result, 2)
+        XCTAssertTrue(result.allSatisfy { $0 == 2 })
     }
 
     func test_leetCodeExample3() {
         let n = 4
         let sut = self.makeSUT()
 
-        let result = sut.fib(n)
+        let result = sut.map { $0.fib(n) }
 
-        XCTAssertEqual(result, 3)
+        XCTAssertTrue(result.allSatisfy { $0 == 3 })
     }
 
     func test_minimumInputSuccess() {
         let n = 0
         let sut = self.makeSUT()
 
-        let result = sut.fib(n)
+        let result = sut.map { $0.fib(n) }
 
-        XCTAssertEqual(result, 0)
+        XCTAssertTrue(result.allSatisfy { $0 == 0 })
     }
 
     func test_maximumInputSuccess() {
         let n = 30
         let sut = self.makeSUT()
 
-        let result = sut.fib(n)
+        let result = sut.map { $0.fib(n) }
 
-        XCTAssertEqual(result, 832_040)
+        XCTAssertTrue(result.allSatisfy { $0 == 832_040 })
     }
 
     func test_minimumInputFailure() {
         let n = -1
         let sut = self.makeSUT()
 
-        let result = sut.fib(n)
+        let result = sut.map { $0.fib(n) }
 
-        XCTAssertEqual(result, -1)
+        XCTAssertTrue(result.allSatisfy { $0 == -1 })
     }
 
     func test_maximumInputFailure() {
         let n = 31
         let sut = self.makeSUT()
 
-        let result = sut.fib(n)
+        let result = sut.map { $0.fib(n) }
 
-        XCTAssertEqual(result, -1)
+        XCTAssertTrue(result.allSatisfy { $0 == -1 })
     }
 
     // MARK: - Helpers
 
-    private func makeSUT() -> FibonacciNumber {
-        FibonacciNumber()
-    }
-}
-
-struct FibonacciNumber {
-    func fib(_ n: Int) -> Int {
-        guard ![0, 1].contains(n) else { return n }
-        guard n > 1, n <= 30 else { return -1 }
-        return self.fib(n - 1) + self.fib(n - 2)
+    private func makeSUT() -> [FibonacciNumberProtocol] {
+        [FibonacciNumber.Recursive(), FibonacciNumber.Dynamic()]
     }
 }
