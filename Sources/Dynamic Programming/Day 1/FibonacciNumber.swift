d1344b885c94ce0b7c171d05f1f1ed58c70091dd
Kamyar Sehati <kamyarsehati@icloud.com>
2022-06-06 13:58:06 +0430

    Day 1 - 1137. N-th Tribonacci Number

--- Sources/Dynamic Programming/Day 1/FibonacciNumber.swift
+++ Sources/Dynamic Programming/Day 1/FibonacciNumber.swift
@@ -12,35 +12,35 @@ protocol FibonacciNumberProtocol {
 }
 
 enum FibonacciNumber {
-    
+
     struct Recursive: FibonacciNumberProtocol {
-        
+
         func fib(_ n: Int) -> Int {
             guard ![0, 1].contains(n) else { return n }
             guard n > 1, n <= 30 else { return -1 }
             return self.fib(n - 1) + self.fib(n - 2)
         }
     }
-    
+
     struct Dynamic: FibonacciNumberProtocol {
-        
+
         func fib(_ n: Int) -> Int {
             guard ![0, 1].contains(n) else { return n }
             guard n > 1, n <= 30 else { return -1 }
-            
+
             var previous = 0
             var result = 1
             var temp = 0
-            
+
             var counter = 2
             while counter <= n {
-                
+
                 temp = previous + result
                 previous = result
                 result = temp
                 counter += 1
             }
-            
+
             return result
         }
     }
